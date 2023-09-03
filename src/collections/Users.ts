65a58620d9125627c567a71bfde9b41b1c032376
Sangwan Jeon <commits@swjeon.kr>
2023-09-03 07:08:43 +0000

    feat: add name to User collection

--- src/collections/Users.ts
+++ src/collections/Users.ts
@@ -1,14 +1,16 @@
-import { CollectionConfig } from 'payload/types';
+import { CollectionConfig } from "payload/types";
 
 const Users: CollectionConfig = {
-  slug: 'users',
+  slug: "users",
   auth: true,
   admin: {
-    useAsTitle: 'email',
+    useAsTitle: "email",
   },
   fields: [
-    // Email added by default
-    // Add more fields as needed
+    {
+      name: "name",
+      type: "text",
+    },
   ],
 };
 
