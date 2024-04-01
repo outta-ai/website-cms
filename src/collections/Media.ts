180be49d9fef4b4a64f23fa05ed03de75d7fdfee
Sangwan Jeon <commits@swjeon.kr>
2024-04-01 16:34:51 +0900

    feat: use custom s3 implementation

--- src/collections/Media.ts
+++ src/collections/Media.ts
@@ -1,12 +1,194 @@
-import type { CollectionConfig } from "payload/types";
+import crypto from "node:crypto";
+import path from "node:path";
+
+import { APIError } from "payload/errors";
+import type {
+	CollectionAfterDeleteHook,
+	CollectionAfterReadHook,
+	CollectionBeforeChangeHook,
+	CollectionConfig,
+	FileData,
+	TypeWithID,
+} from "payload/types";
+
+import { S3Client } from "@aws-sdk/client-s3";
+import { getFiles, type File } from "../utils/getFiles";
+import { S3Create, S3Delete, S3Get } from "../utils/s3";
+
+type S3FileData = FileData &
+	TypeWithID & {
+		object: string;
+	};
+
+// Rename Filename before upload
+const beforeChange: CollectionBeforeChangeHook<S3FileData> = async ({
+	data,
+	req,
+	originalDoc,
+}) => {
+	const bucket = process.env.S3_BUCKET;
+	const client = new S3Client({
+		credentials: {
+			accessKeyId: process.env.S3_ACCESS_KEY_ID,
+			secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
+		},
+		region: process.env.S3_REGION,
+		endpoint: process.env.S3_ENDPOINT,
+	});
+
+	const files = getFiles({ data, req });
+
+	if (!files.length) {
+		return data;
+	}
+
+	if (originalDoc) {
+		const originalFiles: string[] = [];
+
+		if (typeof originalDoc.filename === "string") {
+			originalFiles.push(originalDoc.object);
+		} else if (typeof originalDoc.filename === "object") {
+			originalFiles.push(
+				...Object.values(originalDoc.sizes).map(
+					(resizedFileData) => resizedFileData?.filename,
+				),
+			);
+		}
+
+		const result = await Promise.allSettled(
+			originalFiles.map((filename) =>
+				S3Delete({ client, bucket, key: filename }),
+			),
+		);
+
+		if (result.some((r) => r.status === "rejected")) {
+			throw new APIError(
+				"Failed to delete original files",
+				500,
+				undefined,
+				true,
+			);
+		}
+	}
+
+	const uploadedFiles: File[] = [];
+	for (const file of files) {
+		const ext = path.extname(file.filename);
+		const uuid = crypto.randomUUID();
+		const filename = `${uuid}${ext}`;
+
+		try {
+			await S3Create({ client, file, bucket, key: filename });
+			uploadedFiles.push({ ...file, object: filename });
+		} catch (e: unknown) {
+			throw new APIError("Failed to upload file", 500, undefined, true);
+		}
+	}
+
+	const finalData: S3FileData = {
+		id: data.id,
+		filename: uploadedFiles[0].filename,
+		filesize: uploadedFiles[0].filesize,
+		mimeType: uploadedFiles[0].mimeType,
+		width: uploadedFiles[0].width,
+		height: uploadedFiles[0].height,
+		sizes: {},
+		object: uploadedFiles[0].object,
+	};
+
+	for (const file of uploadedFiles.slice(1)) {
+		finalData.sizes[file.filename] = {
+			filename: file.object,
+			filesize: file.filesize,
+			mimeType: file.mimeType,
+			width: file.width,
+			height: file.height,
+		};
+	}
+
+	return finalData;
+};
+
+// Generate signed URLs after read
+const afterRead: CollectionAfterReadHook<S3FileData> = async ({ doc }) => {
+	if (!doc.filename) {
+		return doc;
+	}
+
+	const bucket = process.env.S3_BUCKET;
+	const client = new S3Client({
+		credentials: {
+			accessKeyId: process.env.S3_ACCESS_KEY_ID,
+			secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
+		},
+		region: process.env.S3_REGION,
+		endpoint: process.env.S3_ENDPOINT,
+	});
+
+	const signedURL = await S3Get({
+		client,
+		bucket,
+		key: doc.object,
+		expiresIn: 15 * 60,
+		roundMinutes: 5,
+	});
+
+	return {
+		...doc,
+		url: signedURL,
+	};
+};
+
+// Handle file deletion
+const afterDelete: CollectionAfterDeleteHook<S3FileData> = async ({ doc }) => {
+	const bucket = process.env.S3_BUCKET;
+	const client = new S3Client({
+		credentials: {
+			accessKeyId: process.env.S3_ACCESS_KEY_ID,
+			secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
+		},
+		region: process.env.S3_REGION,
+		endpoint: process.env.S3_ENDPOINT,
+	});
+
+	const originalFiles: string[] = [];
+
+	if (typeof doc.filename === "string") {
+		originalFiles.push(doc.object);
+	} else if (typeof doc.filename === "object") {
+		originalFiles.push(
+			...Object.values(doc.sizes).map(
+				(resizedFileData) => resizedFileData?.filename,
+			),
+		);
+	}
+
+	const result = await Promise.allSettled(
+		originalFiles.map((filename) =>
+			S3Delete({ client, bucket, key: filename }),
+		),
+	);
+
+	if (result.some((r) => r.status === "rejected")) {
+		throw new APIError("Failed to delete original files", 500, undefined, true);
+	}
+};
 
 const Media: CollectionConfig = {
 	slug: "media",
 	upload: {
 		staticURL: "/media",
-		staticDir: "/media/",
+		staticDir: "/tmp",
 	},
-	fields: [{ name: "alt", type: "text" }],
+	hooks: {
+		beforeChange: [beforeChange],
+		afterRead: [afterRead],
+		afterDelete: [afterDelete],
+	},
+	fields: [
+		{ name: "alt", type: "text" },
+		{ name: "object", type: "text", admin: { hidden: true } },
+	],
 	access: {
 		read: () => true,
 	},
---
+++ src/utils/s3.ts
@@ -0,0 +1,75 @@
+import {
+	DeleteObjectCommand,
+	GetObjectCommand,
+	PutObjectCommand,
+	type S3Client,
+} from "@aws-sdk/client-s3";
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
+import type { File } from "./getFiles";
+
+function roundTime(minutes?: number) {
+	const currentTime = new Date();
+
+	if (minutes === undefined) {
+		return currentTime;
+	}
+
+	currentTime.setMilliseconds(0);
+	currentTime.setSeconds(0);
+	currentTime.setMinutes(
+		Math.floor(currentTime.getMinutes() / minutes) * minutes,
+	);
+
+	return currentTime;
+}
+
+type S3GetArgs = {
+	client: S3Client;
+	bucket: string;
+	key: string;
+	roundMinutes?: number;
+	expiresIn?: number;
+};
+
+export async function S3Get({
+	client,
+	bucket,
+	key,
+	expiresIn,
+	roundMinutes,
+}: S3GetArgs): Promise<string> {
+	const expires = expiresIn || 5 * 60;
+	const command = new GetObjectCommand({ Bucket: bucket, Key: key });
+	return await getSignedUrl(client, command, {
+		expiresIn: expires,
+		signingDate: roundTime(roundMinutes),
+	});
+}
+
+type S3CreateArgs = {
+	client: S3Client;
+	file: File;
+	bucket: string;
+	key: string;
+};
+
+export async function S3Create({ client, file, bucket, key }: S3CreateArgs) {
+	const command = new PutObjectCommand({
+		Bucket: bucket,
+		Key: key,
+		Body: file.buffer,
+		ContentType: file.mimeType,
+	});
+	return await client.send(command);
+}
+
+type S3DeleteArgs = {
+	client: S3Client;
+	bucket: string;
+	key: string;
+};
+
+export async function S3Delete({ client, bucket, key }: S3DeleteArgs) {
+	const command = new DeleteObjectCommand({ Bucket: bucket, Key: key });
+	return await client.send(command);
+}
